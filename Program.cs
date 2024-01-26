704d84a17ab4b6f34a0a6e54f865eb1121911f7e
Morgan Winkler <winkler.morgan1553@gmail.com>
2024-01-26 13:47:54 -0500

    basic logic

--- Program.cs
+++ Program.cs
@@ -1,1 +1,11 @@
-﻿
+﻿// will output a random number from 1-6
+Random dice = new Random();
+int roll = dice.Next(1, 7);
+Console.WriteLine(roll);
+
+// returns the larger of two numbers
+int firstValue = 500;
+int secondValue = 600;
+int largerValue = Math.Max(firstValue, secondValue);
+
+Console.WriteLine(largerValue);

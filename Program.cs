031f215021405ffc129574cdbf41cf69e943eeed
Morgan Winkler <winkler.morgan1553@gmail.com>
2024-01-29 16:43:10 -0500

    array methods

--- Program.cs
+++ Program.cs
@@ -1,29 +1,68 @@
-﻿// data conversion code challenge
+﻿string[] pallets = { "B14", "A11", "B12", "A13" };
 
-string[] values = { "12.3", "45", "ABC", "11", "DEF" };
-
-// Create a looping structure that can be used to iterate through each string value in the array values.
-// Rule 1: If the value is alphabetical, concatenate it to form a message.
-
-// Rule 2: If the value is numeric, add it to the total.
-
-// Rule 3: The result should match the following output:
-
-decimal total = 0m;
-string message = "";
-
-foreach (string value in values)
+Console.WriteLine("Sorted...");
+Array.Sort(pallets);
+foreach (var pallet in pallets)
 {
-  decimal number; // stores the TryParse "out" value
-  if (decimal.TryParse(value, out number))
-  {
-    total += number;
-  }
-  else
-  {
-    message += value;
-  }
+  Console.WriteLine($"-- {pallet}");
 }
 
-Console.WriteLine($"Message: {message}");
-Console.WriteLine($"Total: {total}");
+Console.WriteLine("");
+Console.WriteLine("Reversed...");
+Array.Reverse(pallets);
+foreach (var pallet in pallets)
+{
+  Console.WriteLine($"-- {pallet}");
+}
+
+string[] palletsTwo = { "B14", "A11", "B12", "A13" };
+Console.WriteLine("");
+
+Console.WriteLine($"Before: {palletsTwo[0]}");
+Array.Clear(palletsTwo, 0, 2);
+Console.WriteLine($"After: {palletsTwo[0]}");
+Console.WriteLine($"Clearing 2 ... count: {palletsTwo.Length}");
+foreach (var pallet in palletsTwo)
+{
+  Console.WriteLine($"-- {pallet}");
+}
+Console.WriteLine("");
+// method to create a new array with a different size and copy the elements from the original array to the new one
+Array.Resize(ref palletsTwo, 6);
+Console.WriteLine($"Resizing 6 ... count: {palletsTwo.Length}");
+
+palletsTwo[4] = "C01";
+palletsTwo[5] = "C02";
+
+foreach (var pallet in palletsTwo)
+{
+  Console.WriteLine($"-- {pallet}");
+}
+
+Console.WriteLine("");
+Array.Resize(ref palletsTwo, 3);
+Console.WriteLine($"Resizing 3 ... count: {palletsTwo.Length}");
+
+foreach (var pallet in palletsTwo)
+{
+  Console.WriteLine($"-- {pallet}");
+}
+
+string value = "abc123";
+char[] valueArray = value.ToCharArray();
+Array.Reverse(valueArray);
+string result = new string(valueArray);
+Console.WriteLine(result);
+
+string valueNew = "abc123";
+char[] valueNewArray = valueNew.ToCharArray();
+Array.Reverse(valueNewArray);
+// string result = new string(valueNewArray);
+string resultNew = String.Join(",", valueNewArray);
+Console.WriteLine(resultNew);
+
+string[] items = resultNew.Split(',');
+foreach (string item in items)
+{
+  Console.WriteLine(item);
+}

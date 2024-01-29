ba0c41fb77625b51282b7368bd94648e5c4146d9
Morgan Winkler <winkler.morgan1553@gmail.com>
2024-01-29 16:56:31 -0500

    array challenge

--- Program.cs
+++ Program.cs
@@ -1,68 +1,18 @@
-﻿string[] pallets = { "B14", "A11", "B12", "A13" };
+﻿// Array code challenge Write code to reverse each word in a message
 
-Console.WriteLine("Sorted...");
-Array.Sort(pallets);
-foreach (var pallet in pallets)
+string pangram = "The quick brown fox jumps over the lazy dog";
+
+// expected output: ehT kciuq nworb xof spmuj revo eht yzal god
+
+string[] soloWords;
+soloWords = pangram.Split(" ");
+string[] newSoloWords = new string[soloWords.Length];
+for (int i = 0; i < soloWords.Length; i++)
 {
-  Console.WriteLine($"-- {pallet}");
+  char[] letters = soloWords[i].ToCharArray();
+  Array.Reverse(letters);
+  newSoloWords[i] = new string(letters);
 }
 
-Console.WriteLine("");
-Console.WriteLine("Reversed...");
-Array.Reverse(pallets);
-foreach (var pallet in pallets)
-{
-  Console.WriteLine($"-- {pallet}");
-}
-
-string[] palletsTwo = { "B14", "A11", "B12", "A13" };
-Console.WriteLine("");
-
-Console.WriteLine($"Before: {palletsTwo[0]}");
-Array.Clear(palletsTwo, 0, 2);
-Console.WriteLine($"After: {palletsTwo[0]}");
-Console.WriteLine($"Clearing 2 ... count: {palletsTwo.Length}");
-foreach (var pallet in palletsTwo)
-{
-  Console.WriteLine($"-- {pallet}");
-}
-Console.WriteLine("");
-// method to create a new array with a different size and copy the elements from the original array to the new one
-Array.Resize(ref palletsTwo, 6);
-Console.WriteLine($"Resizing 6 ... count: {palletsTwo.Length}");
-
-palletsTwo[4] = "C01";
-palletsTwo[5] = "C02";
-
-foreach (var pallet in palletsTwo)
-{
-  Console.WriteLine($"-- {pallet}");
-}
-
-Console.WriteLine("");
-Array.Resize(ref palletsTwo, 3);
-Console.WriteLine($"Resizing 3 ... count: {palletsTwo.Length}");
-
-foreach (var pallet in palletsTwo)
-{
-  Console.WriteLine($"-- {pallet}");
-}
-
-string value = "abc123";
-char[] valueArray = value.ToCharArray();
-Array.Reverse(valueArray);
-string result = new string(valueArray);
+string result = String.Join(" ", newSoloWords);
 Console.WriteLine(result);
-
-string valueNew = "abc123";
-char[] valueNewArray = valueNew.ToCharArray();
-Array.Reverse(valueNewArray);
-// string result = new string(valueNewArray);
-string resultNew = String.Join(",", valueNewArray);
-Console.WriteLine(resultNew);
-
-string[] items = resultNew.Split(',');
-foreach (string item in items)
-{
-  Console.WriteLine(item);
-}
--- obj/Debug/net8.0/TestProject.AssemblyInfo.cs
+++ obj/Debug/net8.0/TestProject.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+783e49dd0758f2f840dfed08090a9590f614cfaa")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+031f215021405ffc129574cdbf41cf69e943eeed")]
 [assembly: System.Reflection.AssemblyProductAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

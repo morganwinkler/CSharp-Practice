c32da5f4f1bd6f1d39fff5c5c8e26402cf4e298c
Morgan Winkler <winkler.morgan1553@gmail.com>
2024-01-29 09:57:02 -0500

    nested if/else

--- obj/Debug/net8.0/TestProject.AssemblyInfo.cs
+++ obj/Debug/net8.0/TestProject.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+949b5e1e3e4ee1cde68f89589adfa754afd49a75")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5a741650aff98def452fdd34ad41bb05deb0272e")]
 [assembly: System.Reflection.AssemblyProductAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
